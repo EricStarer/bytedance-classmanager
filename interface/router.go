3fc44d1f30a1f323c648a74257465061f90205b0
xing <1033874657@sjtu.edu.cn>
2022-01-28 19:12:39 +0800

    init interface

---
+++ interface/router.go
@@ -0,0 +1,34 @@
+package types
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRouter(r *gin.Engine) {
+	g := r.Group("/api/v1")
+
+	// 成员管理
+	g.POST("/member/create")
+	g.GET("/member")
+	g.GET("/member/list")
+	g.POST("/member/update")
+	g.POST("/member/delete")
+
+	// 登录
+
+	g.POST("/auth/login")
+	g.POST("/auth/logout")
+	g.GET("/auth/whoami")
+
+	// 排课
+	g.POST("/course/create")
+	g.GET("/course/get")
+
+	g.POST("/teacher/bind_course")
+	g.POST("/teacher/unbind_course")
+	g.GET("/teacher/get_course")
+	g.POST("/course/schedule")
+
+	// 抢课
+	g.POST("/student/book_course")
+	g.GET("/student/course")
+
+}
