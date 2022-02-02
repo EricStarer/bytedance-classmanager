7bd560229565c27cf371e4b6dac2b52dec1237e0
dell <442417060@qq.com>
2022-02-02 18:20:22 +0800

    "update-syp"

--- step3-arrange_course/main.go
+++ step3-arrange_course/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytedance-classmanager/src/types"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -11,10 +10,10 @@ import (
 )
 
 type course struct {
-	ID        string
-	NAME      string
-	CAP       int
-	TeacherId string
+	ID        string `json:"CourseID"`
+	NAME      string `json:"Name"`
+	CAP       int    `json:"Cap"`
+	TeacherId string `json:"TeacherID"`
 }
 
 var err error
@@ -34,13 +33,11 @@ func course_create(c *gin.Context) {
 	var u course
 	c.ShouldBindJSON(&u)
 	db.Create(&u)
-	var t2 course
-	db.Last(&t2)
-	fmt.Printf("%#v\n", t2)
+	db.Last(&u)
 	c.JSON(http.StatusOK, gin.H{
 		"Code": types.OK,
 		"Data": gin.H{
-			"CourseID": t2.ID,
+			"CourseID": u.ID,
 		},
 	})
 }
@@ -49,7 +46,6 @@ func course_create(c *gin.Context) {
 func course_get(c *gin.Context) {
 	var u course
 	c.ShouldBindJSON(&u) //别用错方法，找了半天bug
-	//fmt.Printf("u=%#v\n", u)
 	id, _ := strconv.Atoi(u.ID)
 	db.First(&u, id)
 	if u.NAME == "" {
@@ -72,6 +68,48 @@ func course_get(c *gin.Context) {
 		},
 	})
 }
+
+//绑定课程
+func Bind_Course(c *gin.Context) {
+	var u course
+	c.ShouldBindJSON(&u)
+	course_id, _ := strconv.Atoi(u.ID)
+	teacher_id := u.TeacherId
+	db.First(&u, course_id)
+	if u.TeacherId == "" {
+		db.Debug().Model(&u).Update("TeacherId", teacher_id) //仅修改部分
+		c.JSON(http.StatusOK, gin.H{
+			"Code": types.OK,
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"Code": types.CourseHasBound,
+		})
+	}
+}
+
+//解绑，有疑问:异常情况
+func UnBind_course(c *gin.Context) {
+	var u course
+	c.ShouldBindJSON(&u)
+	course_id, _ := strconv.Atoi(u.ID)
+	db.First(&u, course_id)
+	db.Debug().Model(&u).Update("TeacherId", "") //仅修改部分
+	c.JSON(http.StatusOK, gin.H{
+		"Code": types.OK,
+	})
+}
+
+//查询老师下的所有课程（未完成)，CourseList []*TCourse什么意思？
+func teacher_course_get(c *gin.Context) {
+	var u course
+	var res course[]
+	c.ShouldBindJSON(&u)
+	db.Where("teacher_id=?", u.TeacherId).Find(&res)
+	c.JSON(http.StatusOK, gin.H{
+		"Code": types.OK,
+	})
+}
 func main() {
 	e := connect()
 	//连接失败
@@ -85,9 +123,9 @@ func main() {
 	g.POST("/course/create", course_create)
 	g.GET("/course/get", course_get)
 
-	g.POST("/teacher/bind_course")
-	g.POST("/teacher/unbind_course")
-	g.GET("/teacher/get_course")
+	g.POST("/teacher/bind_course", Bind_Course)
+	g.POST("/teacher/unbind_course", UnBind_course)
+	g.GET("/teacher/get_course", teacher_course_get)
 	g.POST("/course/schedule")
 	r.Run()
 }
