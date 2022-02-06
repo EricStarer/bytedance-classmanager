3b365da10028fef68004ef247339dfc7db1ddfdf
dell <442417060@qq.com>
2022-02-06 21:35:24 +0800

    "add annotation -syp"

--- step3-arrange_course/main.go
+++ step3-arrange_course/main.go
@@ -9,6 +9,7 @@ import (
 	"strconv"
 )
 
+//数据库字段和requeset的请求中（types.go）中相对应
 type course struct {
 	ID        string `json:"CourseID"`
 	NAME      string `json:"Name"`
@@ -31,10 +32,10 @@ func connect() error {
 //创建课程函数
 func course_create(c *gin.Context) {
 	var u course
-	c.ShouldBindJSON(&u)
-	db.Create(&u)
-	db.Last(&u)
-	c.JSON(http.StatusOK, gin.H{
+	c.ShouldBindJSON(&u) //gin框架参数绑定，传入的指针
+	db.Create(&u)        //创建记录
+	db.Last(&u)          //db主键id是自增的,因此查询最新插入的记录,从而获取id值
+	c.JSON(http.StatusOK, gin.H{ //返回值
 		"Code": types.OK,
 		"Data": gin.H{
 			"CourseID": u.ID,
@@ -45,10 +46,10 @@ func course_create(c *gin.Context) {
 //获取课程信息
 func course_get(c *gin.Context) {
 	var u course
-	c.ShouldBindJSON(&u) //别用错方法，找了半天bug
-	id, _ := strconv.Atoi(u.ID)
-	db.First(&u, id)
-	if u.NAME == "" {
+	c.ShouldBindJSON(&u)        //别用错方法，找了半天bug
+	id, _ := strconv.Atoi(u.ID) //将id转为整型，因为数据库中id字段为整型
+	db.First(&u, id)            //查询
+	if u.NAME == "" {           //如果没查询到就返回Errno,CourseNotExisted
 		c.JSON(http.StatusOK, gin.H{
 			"Code": types.CourseNotExisted,
 			"Data": gin.H{
@@ -58,7 +59,7 @@ func course_get(c *gin.Context) {
 			},
 		})
 		return
-	}
+	} //已查询到返回OK
 	c.JSON(http.StatusOK, gin.H{
 		"Code": types.OK,
 		"Data": gin.H{
@@ -72,16 +73,16 @@ func course_get(c *gin.Context) {
 //绑定课程
 func Bind_Course(c *gin.Context) {
 	var u course
-	c.ShouldBindJSON(&u)
-	course_id, _ := strconv.Atoi(u.ID)
+	c.ShouldBindJSON(&u)               //gin，参数绑定
+	course_id, _ := strconv.Atoi(u.ID) //id转为数字，理由同上
 	teacher_id := u.TeacherId
-	db.First(&u, course_id)
-	if u.TeacherId == "" {
+	db.First(&u, course_id) //先查询对应课程的记录
+	if u.TeacherId == "" {  //该课程还未绑定老师，直接更新记录进行绑定
 		db.Debug().Model(&u).Update("TeacherId", teacher_id) //仅修改部分
 		c.JSON(http.StatusOK, gin.H{
 			"Code": types.OK,
 		})
-	} else {
+	} else { //老师字段已经有人了，返回课程已被绑定的ErrNO:CourseHasBound
 		c.JSON(http.StatusOK, gin.H{
 			"Code": types.CourseHasBound,
 		})
@@ -93,8 +94,8 @@ func UnBind_course(c *gin.Context) {
 	var u course
 	c.ShouldBindJSON(&u)
 	course_id, _ := strconv.Atoi(u.ID)
-	db.First(&u, course_id)
-	db.Debug().Model(&u).Update("TeacherId", "") //仅修改部分
+	db.First(&u, course_id)                      //查询对应课程
+	db.Debug().Model(&u).Update("TeacherId", "") //仅修改部分,teacher_id字段修改为空值
 	c.JSON(http.StatusOK, gin.H{
 		"Code": types.OK,
 	})
@@ -103,7 +104,7 @@ func UnBind_course(c *gin.Context) {
 //查询老师下的所有课程（未完成)，CourseList []*TCourse什么意思？
 func teacher_course_get(c *gin.Context) {
 	var u course
-	var res course[]
+	var res []course
 	c.ShouldBindJSON(&u)
 	db.Where("teacher_id=?", u.TeacherId).Find(&res)
 	c.JSON(http.StatusOK, gin.H{
